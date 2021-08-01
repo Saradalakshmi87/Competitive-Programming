63464586ed6037eb5ab113e53f85c0ed82a93650
Sarada Lakshmi Tunuguntla <87535668+Saradalakshmi87@users.noreply.github.com>
2021-08-01 08:25:57 +0530

    Add files via upload

---
+++ repetitiveSum.py
@@ -0,0 +1,61 @@
+'''
+REPETITIVE ADDITION OF DIGITS
+--------------------------------
+
+Given a non-negative integer N, repeatedly add all its digits until the result has only one digit.
+
+Example 1:
+----------
+Input:
+------
+N = 98
+
+Output:
+------
+8
+
+Explanation:
+------------
+Sum of digit of N = 98  is 9 + 8 = 17
+Sum of digit of 17 = 1 + 7 = 8
+Since 8 is the single digit number hence it is the 
+required answer.
+Example 2:
+
+Input:
+------
+N = 1
+
+Output:
+-------
+1
+
+Explanation:
+------------
+Since 1 is the single-digit number hence it is the 
+required answer.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function singleDigit() which takes an integer N as an input parameter and return the single-digit you will get after repeatedly adding the digits of N.
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+------------
+1 <= N <= 1018
+
+'''
+class Solution:
+    def repetitive_sum(self,N):
+        #code here
+        while N > 9:
+            t = N
+            N = 0
+            for i in str(t):
+                N += i
+    return N
+if __name__ == '__main__':
+    n = int(input())
+    ob = Solution()
+    print(ob.repetitive_sum(n))
+
